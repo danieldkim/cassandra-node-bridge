36389c4ad0b6e192ef8d8306fe290349a50b73ec
Daniel Kim <danieldkimster@gmail.com>
2010-06-08 17:46:34 -0400

    switched to node-async-testing as testing framework

--- test/test-misc.js
+++ test/test-misc.js
@@ -10,18 +10,6 @@ var cassandra = require('cassandra-node-client').create(10000, '127.0.0.1', logg
 var sys = require('sys')
 var _ = require('underscore')._
 
-var test_helpers = require('./test-helpers')
-test_helpers.set_logger(logger);
-
-test_helpers.run_async_tests_sequentially([
-  ["Test remove row", test_remove_row],
-  ["Test remove super column", test_remove_super_column],
-  ["Test remove column", test_remove_column],
-  ["Test get_count on super columns", test_get_super_column_count],
-  ["Test get_count on subcolumns", test_get_subcolumn_count],
-  ["Test multiget_slice", test_multiget_slice]
-], setup);
-
 var keyspace = "CassandraNodeBridgeTest"
 var key1 = "test_key1";
 var columns1 = [
@@ -58,6 +46,22 @@ var columns2 = [
 ];
 var insert_ts;
 
+var TestSuite = require('async_testing').TestSuite;
+
+var suite = new TestSuite("Miscellaneous tests");
+suite.setup(setup);
+suite.teardown(teardown);
+suite.addTests({
+  "Test remove row": test_remove_row,
+  "Test remove super column": test_remove_super_column,
+  "Test remove column": test_remove_column,
+  "Test get_count on super columns": test_get_super_column_count,
+  "Test get_count on subcolumns": test_get_subcolumn_count,
+  "Test multiget_slice": test_multiget_slice
+});
+
+suite.runTests();
+
 function setup(test) {
   function mutations_for_columns(columns) {
     var mutations = [];
@@ -90,7 +94,7 @@ function setup(test) {
   mutate_request.send();
 }
 
-function clean_up(clean_up_done) {
+function teardown(clean_up_done) {
   function mutations_for_columns(columns) {
     var mutations = []
     columns.forEach(function(col) {
@@ -118,23 +122,23 @@ function clean_up(clean_up_done) {
   request.send()
 }  
 
-function test_remove_row(test_done) {
+function test_remove_row(assert, finished, test) {
   var column_path = column_parent = { 
     column_family: "TestSuperColumnFamily_UTF8_UTF8"
   }
   
-  _test_remove(test_done, column_path, column_parent)
+  _test_remove(assert, finished, column_path, column_parent)
 }
 
-function test_remove_super_column(test_done) {
+function test_remove_super_column(assert, finished, test) {
   var column_path = column_parent = {
     column_family: "TestSuperColumnFamily_UTF8_UTF8", 
     super_column: columns1[0].name
   }
-  _test_remove(test_done, column_path, column_parent)
+  _test_remove(assert, finished, column_path, column_parent)
 }
 
-function test_remove_column(test_done) {
+function test_remove_column(assert, finished, test) {
   var column_path = {
     column_family: "TestSuperColumnFamily_UTF8_UTF8", 
     super_column: columns1[0].name,
@@ -144,61 +148,55 @@ function test_remove_column(test_done) {
     column_family: "TestSuperColumnFamily_UTF8_UTF8", 
     super_column: columns1[0].name
   }
-  _test_remove(test_done, column_path, column_parent)
+  _test_remove(assert, finished, column_path, column_parent)
 }
 
-function _test_remove(test_done, column_path, column_parent) {
-  var clean_up_wrapper = test_helpers.clean_up_wrapper_factory(clean_up, test_done)
-  var clean_up_after = clean_up_wrapper.clean_up_after  
-  var clean_up_on_exception_for = clean_up_wrapper.clean_up_on_exception_for
-  
+function _test_remove(assert, finished, column_path, column_parent) {
   var request = cassandra.create_request("remove", {
     keyspace: keyspace, key: key1, column_path: column_path, timestamp: Date.now()
   })
   request.addListener("success", function(result) {
-    clean_up_on_exception_for(look_for_it)()
+    look_for_it();
   })
-  request.addListener("error", clean_up_after(function(mess) {
+  request.addListener("error", function(mess) {
     assert.ok(false, "Error removing key: " + mess)              
-  }))
+  })
   request.send();
   
   function look_for_it() {
     var request = cassandra.create_request("get_slice", {
       keyspace: keyspace, key: key1, column_parent: column_parent, 
       predicate: {slice_range: {start:'',finish:'',reversed: false,count:100}}
-    })
-    request.addListener("success", clean_up_after(function(result) {
-      sys.puts("result: " + sys.inspect(result))
+    });
+    request.addListener("success", function(result) {
       assert.equal(0, result.length, "Was expecting no results, but got " + result.length);
-    }))
-    request.addListener("error", clean_up_after(function(mess) {
+      logger.info("get_slice returned no results as expected.")
+      finished();
+    })
+    request.addListener("error", function(mess) {
       assert.ok(false, "Error getting slice: " + mess)              
-    }))
+    });
     request.send();    
   }  
 }
 
-function test_get_super_column_count(test_done) {
-  var clean_up_after = test_helpers.clean_up_wrapper_factory(clean_up, test_done).clean_up_after  
-  
+function test_get_super_column_count(assert, finished, test) {
   var column_parent =  {column_family:"TestSuperColumnFamily_UTF8_UTF8"};
   var request = cassandra.create_request("get_count", {
     keyspace: keyspace, key: key1, column_parent: column_parent
   })
-  request.addListener("success", clean_up_after(function(result) {
+  request.addListener("success", function(result) {
     assert.equal(2, result);
     logger.info("get_count returned correct super column count.")
-  }))
-  request.addListener("error", clean_up_after(function(mess) {
+    finished();
+  });
+  request.addListener("error", function(mess) {
     assert.ok(false, "Error getting super column count: " + mess)              
-  }))
-  request.send()
+  });
+  request.send();
 }
 
-function test_get_subcolumn_count(test_done) {
-  var clean_up_after = test_helpers.clean_up_wrapper_factory(clean_up, test_done).clean_up_after  
-
+function test_get_subcolumn_count(assert, finished, test) {
   var column_parent = { column_family: "TestSuperColumnFamily_UTF8_UTF8", super_column: "k1sc1" }
   var request = cassandra.create_request("get_count", {
     keyspace: keyspace, key: key1, column_parent: column_parent
@@ -208,36 +206,35 @@ function test_get_subcolumn_count(test_done) {
     logger.info("get_count returned correct subcolumn count for sc1.")
     test_sc2();
   })
-  request.addListener("error", clean_up_after(function(mess) {
+  request.addListener("error", function(mess) {
     assert.ok(false, "Error getting subcolumn count: " + mess)              
-  }))
-  request.send()
+  })
+  request.send();
   
   function test_sc2() {
     var column_parent = { column_family: "TestSuperColumnFamily_UTF8_UTF8", super_column: "k1sc2" }
     var request = cassandra.create_request("get_count", {
       keyspace: keyspace, key: key1, column_parent: column_parent
-    })
-    request.addListener("success", clean_up_after(function(result) {
+    });
+    request.addListener("success", function(result) {
       assert.equal(2, result);
-      logger.info("get_count return correct subcolumn count for sc2.")
-    }))
-    request.addListener("error", clean_up_after(function(mess) {
+      logger.info("get_count return correct subcolumn count for sc2.");
+      finished();
+    });
+    request.addListener("error", function(mess) {
       assert.ok(false, "Error getting subcolumn count: " + mess)              
-    }))
-    request.send()
+    });
+    request.send();
   }
 }
 
-function test_multiget_slice(test_done) {
-  var clean_up_after = test_helpers.clean_up_wrapper_factory(clean_up, test_done).clean_up_after  
-
+function test_multiget_slice(assert, finished, test) {
   var column_parent =  {column_family:"TestSuperColumnFamily_UTF8_UTF8"};
   var request = cassandra.create_request("multiget_slice", {
     keyspace: keyspace, keys: [key1, key2], column_parent: column_parent, 
     predicate: {slice_range:{start:'', finish:'', reversed:false, count:5}}
-  })
-  request.addListener("success", clean_up_after(function(result) {
+  });
+  request.addListener("success", function(result) {
     assert.ok(result[key1], "Result did not contain " + key1);
     assert.equal(2, result[key1].length);
     assert.equal(columns1[0].name, result[key1][0].name);
@@ -246,11 +243,12 @@ function test_multiget_slice(test_done) {
     assert.equal(2, result[key2].length);
     assert.equal(columns2[0].name, result[key2][0].name);
     assert.equal(columns2[1].name, result[key2][1].name);
-    logger.info("multiget_slice returned correct results.")
-  }))
-  request.addListener("error", clean_up_after(function(mess) {
+    logger.info("multiget_slice returned correct results.");
+    finished();
+  });
+  request.addListener("error", function(mess) {
     assert.ok(false, "Error requesting multiget_slice: " + mess)              
-  }))
-  request.send()
+  });
+  request.send();
 }
 
